78c09310feb8aa9697da680cbe618d9cde81245c
Kamaia <krystalamaia@gmail.com>
2016-09-13 12:44:04 -0500

    asdf

--- IfTester/Program.cs
+++ IfTester/Program.cs
@@ -8,9 +8,11 @@ using System.Threading.Tasks;
 namespace IfTester {
 	class Program {
 		static void Main( string[ ] args ) {
-			Test t = new Test();
 			for ( int i = 0; i < 10; i++ ) {
+				Console.WriteLine( "Test " + i );
+				Test t = new Test();
 				t.Run( );
+				Console.WriteLine( "====" );
 			}
 			Console.WriteLine( "Press Any Key" );
 			Console.ReadKey( );
--- IfTester/Test.cs
+++ IfTester/Test.cs
@@ -4,65 +4,43 @@ using System.Diagnostics;
 
 namespace IfTester {
 	internal class Test {
-		Dictionary<string, TimeSpan> spans;
-		public Dictionary<string, TimeSpan> Spans { get { return spans; } }
-		int runs = 100000;
-
+		int runs = 100000000;
 		Stopwatch sw;
-
-		public Test( ) {
-			spans = new Dictionary<string, TimeSpan>( );
-		}
 
 		public void Run( ) {
 			sw = new Stopwatch( );
 			sw.Start( );
 			for ( int i = 0; i < runs; i++ ) {
-				FallThrough( );
+				FallThrough( i );
 			}
 			sw.Stop( );
-			Console.WriteLine("Fallthrough: " +  sw.Elapsed );
+			Console.WriteLine( "Fallthrough: " + sw.Elapsed );
 			sw = new Stopwatch( );
 			sw.Start( );
 
 			for ( int i = 0; i < runs; i++ ) {
 
-				NoFallThrough( );
+				NoFallThrough( i );
 			}
 			sw.Stop( );
 			Console.WriteLine( "NoFallthrough: " + sw.Elapsed );
 		}
 
-		private void FallThrough( ) {
-			string match = "Nathan";
-			if ( !( match == "Nathan" ) ) {
+		private void FallThrough( int i ) {
+			if ( i == 0 ) {
 				//skip
 			}
-			else if ( !( match == "Bob" ) ) {
+			else if ( !( i == 0 ) ) {
 				//skip
 			}
-			else if ( !( match == "Jill" ) ) {
-				//skip
-			}
-			else if ( match == "Nathan" ) {
-				//still skip but we're timing here.
-			}
-
 		}
 
-		private void NoFallThrough( ) {
-			string match = "Nathan";
-			if ( match == "Nathan" ) {
+		private void NoFallThrough( int i ) {
+			if ( !( i == 0 )) {
 				//skip
 			}
-			else if ( match == "Bob" ) {
+			else if ( i == 0 ) {
 				//skip
-			}
-			else if ( match == "Jill" ) {
-				//skip
-			}
-			else if ( match == "Kraig" ) {
-				//still skip but we're timing here.
 			}
 		}
 
