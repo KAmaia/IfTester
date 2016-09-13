b8034a8555be8eea75386f33186ae388ca94630b
Kamaia <krystalamaia@gmail.com>
2016-09-13 12:24:03 -0500

    iftest

--- IfTester/Program.cs
+++ IfTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 namespace IfTester {
 	class Program {
 		static void Main( string[ ] args ) {
+			Test t = new Test();
+			for ( int i = 0; i < 10; i++ ) {
+				t.Run( );
+			}
+			Console.WriteLine( "Press Any Key" );
+			Console.ReadKey( );
 		}
 	}
 }
---
+++ IfTester/Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace IfTester {
+	internal class Test {
+		Dictionary<string, TimeSpan> spans;
+		public Dictionary<string, TimeSpan> Spans { get { return spans; } }
+		int runs = 100000;
+
+		Stopwatch sw;
+
+		public Test( ) {
+			spans = new Dictionary<string, TimeSpan>( );
+		}
+
+		public void Run( ) {
+			sw = new Stopwatch( );
+			sw.Start( );
+			for ( int i = 0; i < runs; i++ ) {
+				FallThrough( );
+			}
+			sw.Stop( );
+			Console.WriteLine("Fallthrough: " +  sw.Elapsed );
+			sw = new Stopwatch( );
+			sw.Start( );
+
+			for ( int i = 0; i < runs; i++ ) {
+
+				NoFallThrough( );
+			}
+			sw.Stop( );
+			Console.WriteLine( "NoFallthrough: " + sw.Elapsed );
+		}
+
+		private void FallThrough( ) {
+			string match = "Nathan";
+			if ( !( match == "Nathan" ) ) {
+				//skip
+			}
+			else if ( !( match == "Bob" ) ) {
+				//skip
+			}
+			else if ( !( match == "Jill" ) ) {
+				//skip
+			}
+			else if ( match == "Nathan" ) {
+				//still skip but we're timing here.
+			}
+
+		}
+
+		private void NoFallThrough( ) {
+			string match = "Nathan";
+			if ( match == "Nathan" ) {
+				//skip
+			}
+			else if ( match == "Bob" ) {
+				//skip
+			}
+			else if ( match == "Jill" ) {
+				//skip
+			}
+			else if ( match == "Kraig" ) {
+				//still skip but we're timing here.
+			}
+		}
+
+		private void FallThroughSwitch( ) {
+
+		}
+
+		private void NoFallThroughSwitch( ) {
+
+		}
+	}
+}
